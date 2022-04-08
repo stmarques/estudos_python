240e38d70c1c466dd2e6fe6e6bc1e2c552707bc4
Amanda <83442480+stmarques@users.noreply.github.com>
2022-04-08 00:10:39 -0300

    Update aula_2.py

--- aula_2.py
+++ aula_2.py
@@ -48,7 +48,9 @@ variavel1=input("insira valor 1")
 variavel2=input("insira valor 2")
 print(variavel1+variavel2,type(variavel1+variavel2))
 
-#4. OPERADORES BÁSICOS
+#4. OPERADORES
+
+#4.1. OPERADORES ARITMÉTICOS
 
 #EXEMPLO 1
 variavel1=input("insira o valor 1: ")
@@ -57,25 +59,25 @@ variavel2=input("insira o valor 2: ")
 variavel1=int(variavel1)
 variavel2=int(variavel2)
 
-print("Soma: ",variavel1+variavel2,type(variavel1+variavel2))
+print("Adição: ",variavel1+variavel2,type(variavel1+variavel2))
 print("Subtração: ",variavel1-variavel2,type(variavel1-variavel2))
 print("Multiplicação: ",variavel1*variavel2,type(variavel1*variavel2))
 print("Divisão: ",variavel1/variavel2,type(variavel1*variavel2))
 print("Divisão inteira: ",variavel1//variavel2,type(variavel1//variavel2))
-print("Modulo: ",variavel1%variavel2,type(variavel1%variavel2))
-print("Exponencial: ",variavel1**variavel2,type(variavel1**variavel2))
+print("Módulo: ",variavel1%variavel2,type(variavel1%variavel2)) #retorna o resto da divisão de ambos operandos
+print("Exponenciação: ",variavel1**variavel2,type(variavel1**variavel2)) #retorna o resultado da elevação da potência pelo outro
 
 #EXEMPLO 2
 variavel1=int(input("Insira o valor 1:"))
 variavel2=int(input("Insira o valor 2"))
 
-print("Soma: ",variavel1+variavel2,type(variavel1+variavel2))
+print("Adição: ",variavel1+variavel2,type(variavel1+variavel2))
 print("Subtração: ",variavel1-variavel2,type(variavel1-variavel2))
 print("Multiplicação: ",variavel1*variavel2,type(variavel1*variavel2))
 print("Divisão: ",variavel1/variavel2,type(variavel1*variavel2))
 print("Divisão inteira: ",variavel1//variavel2,type(variavel1//variavel2))
-print("Modulo: ",variavel1%variavel2,type(variavel1%variavel2))
-print("Exponencial: ",variavel1**variavel2,type(variavel1**variavel2))
+print("Módulo: ",variavel1%variavel2,type(variavel1%variavel2)) #retorna o resto da divisão de ambos operandos
+print("Exponenciação: ",variavel1**variavel2,type(variavel1**variavel2)) #retorna o resultado da elevação da potência pelo outro
 
 #EXEMPLO 3
 variavel1=bool(input("insira valor 1: "))
@@ -99,7 +101,7 @@ variavel6=int(input("Insira o valor 6:"))
 variavel5/=variavel6
 print("Divisão =: ",variavel5,type(variavel5))
 
-#5. PRECEDÊNCIA DE OPERADORES
+#4.1.2. PRECEDÊNCIA DE OPERADORES
 
 #EXEMPLO 1
 variavel1=input("insira o valor 1: ")
@@ -125,5 +127,65 @@ total2=valor1 * (valor2 + valor3)
 
 print("Total 1: ", total1, type(total1), "Total 2:", total2, type(total2))
 
-#6. OPERADORES RELACIONAIS
+#4.2. OPERADORES RELACIONAIS OU DE COMPARAÇÃO
 
+variavel1=input("Insira o valor 1: ")
+variavel2=input("Insira o valor 2: ")
+
+#IGUAL A ==
+resultado = variavel1 == variavel2
+print("resultado ==:", resultado, type(resultado))
+
+#DIFERENTE DE !=
+resultado = variavel1 != variavel2
+print("Resultado !=:", resultado, type(resultado))
+
+#MENOR QUE <
+resultado = variavel1 < variavel2
+print("Resultado <:", resultado, type(resultado))
+
+#MAIOR QUE >
+resultado = variavel1 > variavel2
+print("Resultado >:", resultado, type(resultado))
+
+#MAIOR OU IGUAL A >=
+resultado = variavel1 >= variavel2
+print("Resultado >=:", resultado, type(resultado))
+
+#MENOR OU IGUAL A <=
+resultado = variavel1 <= variavel2
+print("Resultado <=:", resultado, type(resultado))
+
+#4.3. OPERADORES LÓGICOS
+
+variavel1=input("insira o valor: ")
+variavel2=input("insira o valor: ")
+variavel3=input("insira o valor: ")
+
+#AND (EM OUTRAS LINGUAGENS É O EQUIVALENTE A & &&)
+resultado = variavel1==variavel2 and variavel2==variavel3
+print("resultado == and:", resultado, type(resultado))
+
+#OR (EM OUTRAS LINGUAGENS É O EQUIVALENTE A │ ││)
+resultado = variavel1==variavel2 or variavel2==variavel3
+print("resultado == or:", resultado, type(resultado))
+
+#OR NOT
+resultado = variavel1==variavel2 or not variavel2==variavel3
+print("resultado == or not:", resultado, type(resultado))
+
+
+#OPERADOR DE IDENTIDADE
+
+
+#OPERADOR DE ASSOCIAÇÃO
+variavel1=input("insira o valor: ")
+lista="Vidas Secas"
+
+#IN
+resultado=variavel1 not in lista
+print("resultado not in: ", resultado)
+
+#NOT IN
+resultado=variavel1 in lista
+print("resultado in: ", resultado)
