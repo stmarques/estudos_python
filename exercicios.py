f319349a361c1dcf14fc9c049e4d5c58fc1c1111
Amanda <83442480+stmarques@users.noreply.github.com>
2022-04-14 03:03:03 -0300

    Create exercicios.py

---
+++ exercicios.py
@@ -0,0 +1,21 @@
+'''
+EXERCÍCIO 1
+O estatudo de uma ONG determina que todas as doações recebidas devem gerar um valor para investimento, para cobrir momentos de necessidade.
+O valor investido deve ser de 5% da doação. Porém, em casos em que as doações ultrapassem R$ 1000,00 o investimento deve ser de 15% da doação.
+Sua missão é criar um programa capaz de fazer os cálculos necessários e indicar quanto deve ser investido.
+
+Como resolver este problema:
+
+Primeiro é preciso pensar quais são as entradas de dados e as saídas de informações.
+No vaso, a situação apresentada possui três variáveis. 
+1ª valor das doação
+2ª valor do investimento
+3ª valor do caixa
+
+Em seguida analisamos qual é o tipo de verificação ou operação a ser executada.
+No caso, os investimos devem de 5% por doação ou 15% para doações acima de R$ 1000,00
+Se o calculo for superior a R$ 1000,00 será preciso multiplicar por 0.15 e armazenar o resultado dentro na variável investimento.
+Depois é preciso pegar o valor da doação e subtrair pelo valor do investinmento e armazenar o resultado na variável caixa.
+
+'''
+
