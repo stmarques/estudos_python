a3177cc97ce47da17e4c4895a2b8fab00d9dd8cc
Amanda <83442480+stmarques@users.noreply.github.com>
2022-04-17 18:32:53 -0300

    Update exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -17,9 +17,6 @@ No caso, os investimos devem de 5% por doação ou 15% para doações acima de R
 Se o calculo for superior a R$ 1000,00 será preciso multiplicar por 0.15 e armazenar o resultado dentro na variável investimento.
 Depois é preciso pegar o valor da doação e subtrair pelo valor do investinmento e armazenar o resultado na variável caixa.
 
-'''
-
-'''
 EXERCÍCIO 2
 Uma loja de ferragens oferece descontos conforme duas situações:
     
@@ -27,9 +24,7 @@ Uma loja de ferragens oferece descontos conforme duas situações:
     b) se a compra tiver mais de 10 itens distintos o cliente recebe 5% de desconto.
 
 Verificar e demonstrar o desconto (caso exista) e o valaor total da compra.
-'''
 
-'''
 EXERCÍCIO 3
 Qual o valor da variável [d] após a execução do código Python abaixo?
 a = 2
