d71077feef34133267946563f0cbdaec86e13e9b
Amanda <83442480+stmarques@users.noreply.github.com>
2022-04-17 18:34:55 -0300

    Update and rename resolucoes to resolucoes.py

---
+++ resolucoes.py
@@ -0,0 +1,15 @@
+#EXERCÍCIO 1
+#EXERCÍCIO 2
+#EXERCÍCIO 3
+a=2
+b=4
+c=8
+d=0
+if((a>=2) or (b<=3)):
+    d=(a+c)/2;
+    print("d = {} ".format(d))
+
+else:
+    d=b*c;
+    print("d = {}".format(d))
+   
