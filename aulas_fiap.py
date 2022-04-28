e743493ac463bb3e569ed80593f18308e11f3e98
Amanda <83442480+stmarques@users.noreply.github.com>
2022-04-27 22:11:06 -0300

    Update aulas_fiap.py

--- aulas_fiap.py
+++ aulas_fiap.py
@@ -1,15 +1,15 @@
-#ATRIBUIR VARIÁVEIS#
+#ATRIBUIR VARIÁVEIS
 variavel = "Olá, mundo!"
 print(variavel)
 
-#ATRIBUIR VARIÁVEIS E SUBTRAIR#
+#EXEMPLO
 nome = input ("Digite seu nome: ")
 ano_nascimento = input ("Digite seu ano de nascimento: ")
 ano_atual = input ("Digite o ano atual: ")
 idade = int(ano_atual) - int(ano_nascimento)
 print(idade)
 
-#1. TIPOS DE VARIAVEIS
+#TIPOS DE VARIAVEIS
 
 #str (string): texto
 valorString = "nome"
